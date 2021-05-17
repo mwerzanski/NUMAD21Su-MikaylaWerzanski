85037a1ceefbfb0da8b0455670e0906c41451381
mikaylawerzanski <werzanski.m@northeastern.edu>
2021-05-16 20:15:18 -0400

    added button onClick to view contact information

--- app/src/main/java/com/example/numad21su_mikaylawerzanski/MainActivity.java
+++ app/src/main/java/com/example/numad21su_mikaylawerzanski/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.numad21su_mikaylawerzanski;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button aboutBtn = (Button) findViewById(R.id.aboutBtn);
+        aboutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Mikayla Werzanski \n" +
+                        "Contact: werzanski.m@northeastern.edu", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
