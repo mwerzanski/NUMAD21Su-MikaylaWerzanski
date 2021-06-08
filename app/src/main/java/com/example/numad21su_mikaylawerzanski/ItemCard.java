a63b2e62c92b0b1fc21cbdac2cf8d9949dbc4160
mikaylawerzanski <werzanski.m@northeastern.edu>
2021-06-07 22:22:23 -0400

    Add initial ItemCard

---
+++ app/src/main/java/com/example/numad21su_mikaylawerzanski/ItemCard.java
@@ -0,0 +1,27 @@
+package com.example.numad21su_mikaylawerzanski;
+
+public class ItemCard implements ItemClickListener {
+
+    private final String name;
+    private final String link;
+
+    //Constructor
+    public ItemCard(String name, String link) {
+        this.name = name;
+        this.link = link;
+    }
+
+    //Getters for item name and link
+    public String getName() {
+        return name;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        String name = getName();
+    }
+}
