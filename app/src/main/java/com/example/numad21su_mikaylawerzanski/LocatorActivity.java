84e969027822209f59b23b3ce5073b48d9082981
mikaylawerzanski <werzanski.m@northeastern.edu>
2021-06-11 10:14:54 -0400

    create initial locator activity

---
+++ app/src/main/java/com/example/numad21su_mikaylawerzanski/LocatorActivity.java
@@ -0,0 +1,14 @@
+package com.example.numad21su_mikaylawerzanski;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LocatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_locator);
+    }
+}
