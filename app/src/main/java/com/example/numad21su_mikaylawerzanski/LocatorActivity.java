d4254433a671c571cc2ec1e1e45aeec275df1a74
mikaylawerzanski <werzanski.m@northeastern.edu>
2021-06-13 23:53:51 -0400

    Add locator activity

--- app/src/main/java/com/example/numad21su_mikaylawerzanski/LocatorActivity.java
+++ app/src/main/java/com/example/numad21su_mikaylawerzanski/LocatorActivity.java
@@ -1,14 +1,38 @@
 package com.example.numad21su_mikaylawerzanski;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LocatorActivity extends AppCompatActivity {
+
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_locator);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        double longitude = location.getLongitude();
+        TextView lngtd = (TextView) findViewById(R.id.longitude);
+        lngtd.setText("Longitude: " + Double.toString(longitude));
+        double latitude = location.getLatitude();
+        TextView lat = (TextView) findViewById(R.id.latitude);
+        lat.setText("Latitude: " + Double.toString(latitude));
     }
 }
